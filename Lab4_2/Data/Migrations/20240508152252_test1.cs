f32d230757b2aff04f97419550c6c33311e01f91
Łukasz Sokołowski <264257@student.pwr.edu.pl>
2024-05-08 18:27:38 +0200

    Final Lab4_2 version

---
+++ Lab4_2/Data/Migrations/20240508152252_test1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lab4_2.Migrations
+{
+    /// <inheritdoc />
+    public partial class test1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "AvgRating",
+                table: "Games",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "AvgRating",
+                table: "Games",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+        }
+    }
+}
